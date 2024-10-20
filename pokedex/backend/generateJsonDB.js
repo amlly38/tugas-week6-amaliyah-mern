daa49c0c367ec0439268ea2e19c5be13c0e8d6e3
amlly38 <amaliyahamel0308@gmail.com>
2024-10-20 20:05:04 +0700

    Membuat payload kosong untuk menyimpan Data Pokemon pada generateJsonDB.js -pokedex/backend

--- pokedex/backend/generateJsonDB.js
+++ pokedex/backend/generateJsonDB.js
@@ -8,7 +8,8 @@ async function generateJsonDB() {
   try {
     const pokemonApiURL = "https://pokeapi.co/api/v2/pokemon/?limit=100";
     const pokemonList = await fetch(pokemonApiURL).then((res) => res.json());
-    
+    const payload = [];
+
   } catch (error) {
     console.log(error);
   }  

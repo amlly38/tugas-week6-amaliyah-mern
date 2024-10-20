1d1f0f00d660ae43477eb3dc507d180457e64260
amlly38 <amaliyahamel0308@gmail.com>
2024-10-20 20:02:23 +0700

    Mengambil Data Pokemon dari API pada generateJsonDB.js -pokedex-backend

--- pokedex/backend/generateJsonDB.js
+++ pokedex/backend/generateJsonDB.js
@@ -6,7 +6,9 @@ async function generateJsonDB() {
   // dan ketika akses url http://localhost:3000/pokemon akan muncul seluruh data
   // pokemon yang telah kalian parsing dari public api pokemon
   try {
-    // kode di sini akan dicoba
+    const pokemonApiURL = "https://pokeapi.co/api/v2/pokemon/?limit=100";
+    const pokemonList = await fetch(pokemonApiURL).then((res) => res.json());
+    
   } catch (error) {
     console.log(error);
   }  
